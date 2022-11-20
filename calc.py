862e62d74a1a7eca65b166f82b74dddaf77d9063
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-19 23:36:53 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Calculator")
 
-user_text = st.text_input("Input Your Problem")
+user_text = st.text_input("Input Your Problem", Value = "here", max_chars = None,)
 if st.button("9"):
     st.text_input(9)
