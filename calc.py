a4970e5d413d3186d27efbf8a6df993c97d5c4a0
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-20 12:34:07 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -10,12 +10,12 @@ logtxtbox.text_area("Enter Your Problem",logtxt)
 Button_9 = st.button('9', key=1)
 Button_8 = st.button('8', key=2)
 Button_Enter = st.button('Enter', key=0)
-if Button_9:
-    logtxt += '9'
-    logtxtbox.text_area("Enter Your Problem", logtxt,)
-if Button_8:
-    logtxt += '8'
-    logtxtbox.text_area("Enter Your Problem", logtxt,)
+
 while(Button_Enter):
-    logtxt += ''
-    logtxtbox.text_area("Enter Your Problem", logtxt, )
+    if Button_9:
+        logtxt += '9'
+        logtxtbox.text_area("Enter Your Problem", logtxt,)
+    if Button_8:
+        logtxt += '8'
+        logtxtbox.text_area("Enter Your Problem", logtxt,)
+
