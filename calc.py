5517011e6dab8aad6ee93af32ffd6681397081cb
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-20 00:07:08 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("Calculator")
 
-user_text = st.text_input('Input Your Problem', max_chars=None)
+placeholder = st.empty()
+
+user_input = placeholder.text_input('text')
 button_9 = st.button('9', key=3)
 if button_9:
-    user_text = st.text_input('', value='9', key=3,)
+    user_input = placeholder.text_input('', value='9', key=3,)
