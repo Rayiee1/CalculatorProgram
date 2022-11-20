36be09ed29b6264830ad1584b3ade54189ff1b63
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-20 12:29:46 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Calculator')
 
 # creating a placeholder for the fixed sized textbox
-logtxtbox = st.text_input()
+logtxtbox = st.empty()
 logtxt = ''
 logtxtbox.text_area("Enter Your Problem",logtxt)
 
