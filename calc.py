063bd482b4d52a7c423ccfc0798dc25cbd9d7f28
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-20 12:24:37 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -5,8 +5,9 @@ st.title('Calculator')
 # creating a placeholder for the fixed sized textbox
 logtxtbox = st.empty()
 logtxt = ''
+logtxtbox.text_area("Enter Your Problem",logtxt)
 
 Button_9 = st.button('9', key=1)
 if Button_9:
     logtxt += '9'
-    logtxtbox.text_area("Proble Here", logtxt,)
+    logtxtbox.text_area("Enter Your Problem", logtxt,)
