74e888bfe7080566f475a613a59fd6cbc488c3f3
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-20 11:47:31 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ st.title("Calculator")
 placeholder = st.empty()
 
 user_input = placeholder.text_input('Input Problem Here')
-button_9 = st.button('9', key=3)
-button_8 = st.button('8', key=2)
+button_9 = st.button('9', key=9)
+button_8 = st.button('8', key=8)
 if button_9:
-    user_input = placeholder.text_input('', value='9', key=1,)
+    user_input = placeholder.text_input('', value='9', key=9,)
