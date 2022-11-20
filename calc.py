beec911aa764c9d111f0a891cecfd3bf1f575ca5
Rayiee1 <92122405+Rayiee1@users.noreply.github.com>
2022-11-20 11:54:35 -0500

    hello my name is ray

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ st.title("Calculator")
 
 placeholder = st.empty()
 
-user_input = placeholder.number_input()
+user_input = st.number_input("Enter Your Problem")
 button_9 = st.button('9', key=9)
 button_8 = st.button('8', key=8)
 if button_9:
